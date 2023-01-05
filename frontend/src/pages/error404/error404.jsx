08fd872b99ec7e4fef4e965e7d40c8136d1ae514
queva <queval.loic@gmail.com>
2023-01-05 19:31:30 +0100

    page 404

--- frontend/src/pages/error404/error404.jsx
+++ frontend/src/pages/error404/error404.jsx
@@ -1,15 +1,14 @@
 import {useRouteError} from 'react-router-dom';
+import './error404.scss';
 
 export const Error404 = () => {
     const error = useRouteError();
     console.error(error);
     return (
         <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.status} {error.statusText}</i>
-            </p>
+            <h2>404</h2>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <a href="/">Retourner sur la page d'accueil</a>
         </div>
     );
 }
