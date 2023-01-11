3eac2079c3c22f1782ab2e99bc72f1b455f57abe
queva <queval.loic@gmail.com>
2023-01-11 17:48:50 +0100

    Renane variable

---
+++ frontend/src/components/collapse/collapse.jsx
@@ -0,0 +1,9 @@
+import './collapse.scss'
+
+export const Collapse = () => {
+    return (
+        <div>
+            <p>Collapse</p>
+        </div>
+    );
+}
--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -1,11 +1,12 @@
 import {Banner} from '../../components/banner/banner';
+import {Collapse} from '../../components/collapse/collapse';
 import banner from '../../assets/images/banners/about_banner.png'
 
 export const About = () => {
     return (
         <main>
             <Banner text="" banner={banner}/>
-            <h1>About</h1>
+            <Collapse/>
         </main>
     );
 }
